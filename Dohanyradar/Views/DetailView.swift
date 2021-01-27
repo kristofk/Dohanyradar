f107b2b2252fd4e2b17372a25b51e93022e26d4a
Kristof Kocsis <kocsis.kris@gmail.com>
2021-01-27 15:15:53 +0100

    DetailView

---
+++ Dohanyradar/Views/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  Dohanyradar
+//
+//  Created by Kristof Kocsis on 1/27/21.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}

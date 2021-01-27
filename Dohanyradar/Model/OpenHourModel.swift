455b57b7478fd2fba26366239bc2bd792ef80cd3
Kristof Kocsis <kocsis.kris@gmail.com>
2021-01-27 11:57:36 +0100

    Added BaseProject Api Models

---
+++ Dohanyradar/Model/OpenHourModel.swift
@@ -0,0 +1,14 @@
+//
+//  OpenHourModel.swift
+//  Dohanyradar
+//
+//  Created by Kristof Kocsis on 1/27/21.
+//
+
+import Foundation
+
+struct OpenHourModel: Codable {
+    let day: Int
+    let openTime: String
+    let closeTime: String
+}
---
+++ Dohanyradar/Model/TobaccoShopListModel.swift
@@ -0,0 +1,17 @@
+//
+//  TobaccoShopListModel.swift
+//  Dohanyradar
+//
+//  Created by Kristof Kocsis on 1/27/21.
+//
+
+import Foundation
+
+struct TobaccoShopListModel: Codable {
+    let id: Int
+    let name: String
+    let address: String
+    let longitude: Double
+    let latitude: Double
+    let isOpen: Bool
+}

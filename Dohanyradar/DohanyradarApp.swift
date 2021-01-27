1898d3164f7cb00e59eed137d6395616cbdf2f5c
Kristof Kocsis <kocsis.kris@gmail.com>
2021-01-27 11:21:14 +0100

    Initial Commit

---
+++ Dohanyradar/DohanyradarApp.swift
@@ -0,0 +1,17 @@
+//
+//  DohanyradarApp.swift
+//  Dohanyradar
+//
+//  Created by Kristof Kocsis on 1/27/21.
+//
+
+import SwiftUI
+
+@main
+struct DohanyradarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

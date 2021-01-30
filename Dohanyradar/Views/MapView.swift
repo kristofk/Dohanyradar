533384379cfdf267d255c73e9ba0b80c1da59544
Kristof Kocsis <kocsis.kris@gmail.com>
2021-01-30 15:36:31 +0100

    ShopMapView

--- Dohanyradar/Views/MapView.swift
+++ Dohanyradar/Views/MapView.swift
@@ -8,34 +8,13 @@
 import SwiftUI
 import MapKit
 
-struct City: Identifiable {
-    let id = UUID()
-    let coordinate: CLLocationCoordinate2D
-}
-
 struct MapView: View {
     
     @EnvironmentObject var shops: Shops
     
-    @State var coordinateRegion = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 47.497913, longitude: 19.040236),
-        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
-    
     var body: some View {
         NavigationView() {
-            Map(coordinateRegion: $coordinateRegion, annotationItems: shops.shops) { shop in
-                MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: shop.latitude, longitude: shop.longitude)) {
-                    
-                    NavigationLink(destination: DetailView(id: shop.id)) {
-                        ZStack {
-                            Image(systemName: "mappin")
-                            Text(shop.name).offset(x: 0, y: 15)
-                        }
-                        .padding(.vertical, 15)
-                    }
-                    
-                }
-            }
+            ShopMapView(shops: shops.shops)
             .navigationTitle("")
             .navigationBarHidden(true)
             .ignoresSafeArea()
